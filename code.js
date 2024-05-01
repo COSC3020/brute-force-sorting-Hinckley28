8e2b318c39fe3c25fd8e8ce6c01ed9548a221270
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-05-01 15:10:48 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
-function permutationSort(a) {
-    return 0;
+
+function permutationSort(a, l = 0, r = a.length - 1, count = 0) {
+  if (isSorted(a)) {
+    return count;
+  }
+    if (l === r) {
+      //console.log(a);
+      count++;
+    }
+    else {
+      for (let i = r; i >= l; i--) { 
+        swap(a, i, r); 
+        count = permutationSort(a, l, r - 1, count);
+        if (!isSorted(a)) {
+          swap(a, i, r); 
+        }
+      }
+    }
+    return count;
+}
+
+function isSorted(a) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > a[i + 1]) {
+      return false;
+    }
+  }
+  //console.log('sorted array is ' +a);
+  return true;
+}
+
+function swap(a, i, j) {
+  const temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+  //console.log('after swap' +a);
+  return a;
 }
